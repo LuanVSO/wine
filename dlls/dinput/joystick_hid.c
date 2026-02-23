06d8bc5e19b2ba0da7fb2c332f263ec6c8971033
Luan Vitor Simião oliveira <luanv.oliveira@outlook.com>
2026-02-23 07:40:48 -0300

    Add Logitech G29 wheel to custom mapping override
    the g29 also follows the same remapping for g920

--- dlls/dinput/joystick_hid.c
+++ dlls/dinput/joystick_hid.c
@@ -49,6 +49,7 @@
 
 #define VID_LOGITECH 0x046D
 #define PID_LOGITECH_G920 0xC262
+#define PID_LOGITECH_G29 0xC24F
 
 WINE_DEFAULT_DEBUG_CHANNEL(dinput);
 
@@ -625,7 +626,7 @@ static BOOL enum_objects( struct hid_joystick *impl, const DIPROPHEADER *filter,
             case MAKELONG(HID_USAGE_GENERIC_RX, HID_USAGE_PAGE_GENERIC):
             case MAKELONG(HID_USAGE_GENERIC_RY, HID_USAGE_PAGE_GENERIC):
             case MAKELONG(HID_USAGE_GENERIC_RZ, HID_USAGE_PAGE_GENERIC):
-                if (!impl->wgi_device && impl->attrs.VendorID == VID_LOGITECH && impl->attrs.ProductID == PID_LOGITECH_G920)
+                if (!impl->wgi_device && impl->attrs.VendorID == VID_LOGITECH && (impl->attrs.ProductID == PID_LOGITECH_G920 || impl->attrs.ProductID == PID_LOGITECH_G29))
                 {
                     if (j == HID_USAGE_GENERIC_X)
                     {
@@ -651,7 +652,7 @@ static BOOL enum_objects( struct hid_joystick *impl, const DIPROPHEADER *filter,
                         hack_guid = &GUID_Slider;
                         hack_name = L"Clutch";
                     }
-                    else WARN("unknown axis usage page %x usage %lx for Logitech G920\n", caps->usage_page, j);
+                    else WARN("unknown axis usage page %x usage %lx for Logitech g29/G920\n", caps->usage_page, j);
                 }
                 else
                 {
@@ -1684,7 +1685,7 @@ static HRESULT hid_joystick_device_try_open( const WCHAR *path, HANDLE *device, 
         break;
     }
 
-    if (attrs->VendorID == VID_LOGITECH && attrs->ProductID == PID_LOGITECH_G920)
+    if (attrs->VendorID == VID_LOGITECH && (attrs->ProductID == PID_LOGITECH_G920 || attrs->ProductID == PID_LOGITECH_G29))
         type = DI8DEVTYPE_DRIVING | (DI8DEVTYPEDRIVING_DUALPEDALS << 8);
 
     instance->dwDevType = device_type_for_version( type, version ) | DIDEVTYPE_HID;
